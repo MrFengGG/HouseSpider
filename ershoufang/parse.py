0e7c3deb5124a314546037e5f058bbd9324931fb
MRFENG <763484204@qq.com>
2017-12-05 17:15:08 +0800

    将解析地址的方法升级为异步请求,大大提高速度

--- ershoufang/parse.py
+++ ershoufang/parse.py
@@ -1,9 +1,17 @@
 #coding=utf-8
 import pymongo
+from gevent.pool import Pool
 import urllib.request
 import json
 import urllib.parse
-from parseSettings import * 
+import socket
+from gevent import monkey
+from parseSettings import *
+import gevent
+import time
+import datetime
+monkey.patch_all()	#修改标准库
+socket.setdefaulttimeout(2) 
 class LocationParser(object):
 	#用于地址解析的类
 	def __init__(self,ak,baseUrl):
@@ -14,39 +22,50 @@ class LocationParser(object):
 		url = self.baseUrl+"?ak="+self.ak+"&address="+urllib.parse.quote(address)+"&output="+output
 		if city:
 			url = url + "&city="+urllib.parse.quote(city)
-		result = urllib.request.urlopen(url)
+		try:
+			result = urllib.request.urlopen(url)
+		except:
+			print("连接错误")
+			return None,url
 		jsonresult = json.load(result)
 		return jsonresult,url
 
-class dumpLocation():
-	def __init__(self,ip,port):
+class LocationDumper():
+	def __init__(self,ip,port,locationDB,locationCol,sourceDB,sourceCol,requestNum = 10,city = None):
+		#数据库连接
 		self.client = pymongo.MongoClient(ip,port)
+		#地址解析器
 		self.bParser = LocationParser("vxc9SN7XHCI1M6y8djyBD98Upx5CCprX",
 																  "http://api.map.baidu.com/geocoder/v2/")
-	def getDatas(self,dbName,colName):
-		col = self.client[dbName][colName]
-		datas = col.find({"status":False})
-		return datas
-	def parse(self,city,location):
-		datas = self.getDatas("ershoufang",city)
-		col = self.client["ershoufang"][location]
-		for data in datas:
-			if col.count({"_id":data["_id"]}) <= 0:
-				result = self.bParser.parse(data["address"],"json",data["city"])
-				jsonResult = result[0]
-				if jsonResult['status'] != 0:
-					result = self.bParser.parse(data["address"],"json")
-				jsonResult = result[0]
-				urlResult = result[1]
-				ln = ''
-				lat = ''
-				if jsonResult['status'] == 0:
-					ln = jsonResult['result']['location']['lng']
-					lat = jsonResult['result']['location']['lat']
-				else:
-					print("地址解析错误,status:"+str(jsonResult['status'])+",msg:"+jsonResult['msg']+",errorUrl:"+urlResult)
-					continue
-				try:
+		#并发数量控制
+		self.requestNum = requestNum
+		#带解析数据集合
+		self.sourceCol = self.client[sourceDB][sourceCol]
+		#用于存储结果的集合
+		self.locationCol = self.client[locationDB][locationCol]
+		self.oklength = 0
+		self.errorlength = 0
+	#用于解析每一条数据的方法
+	def parseData(self,data):
+		#数据集中没有带解析的数据,解析该条数据
+		if self.locationCol.count({"_id":data["_id"]}) <= 0:
+			result = self.bParser.parse(data["address"],"json",data["city"])
+			#根据返回码判断解析结果是否正确,如果不正确,去掉城市参数重试
+			if result[0] and result[0]['status'] != 0:
+				result = self.bParser.parse(data["address"],"json")
+			jsonResult = result[0]
+			urlResult = result[1]
+			ln = None
+			lat = None
+			if jsonResult['status'] == 0:
+				ln = jsonResult['result']['location']['lng']
+				lat = jsonResult['result']['location']['lat']
+			else:
+				print("地址解析错误,status:"+str(jsonResult['status'])+",msg:"+jsonResult['msg']+",errorUrl:"+urlResult)
+				self.sourceCol.update({"_id":data["_id"]},{"$set":{"status":"ERROR"}})
+				self.errorlength += 1
+				return
+			try:
 					a = {'_id':data['_id'],
 								"size":data['size'],
 								"orient":data['orient'],
@@ -59,19 +78,38 @@ class dumpLocation():
 								"address":data['address'],
 								"time":data['nowTime'],
 								"city":data['city']}	
-				except:
-					print("存储数据异常,删除该条数据")
-					self.client['ershoufang'][city].remove({"_id":data['_id']})
-					continue
-				col.insert(a);
-				print("成功插入一条数据"+data['address'])
-				self.client['ershoufang'][city].update({"_id":data['_id']},{"$set":{"status":True}})	
-			else:
-				self.client['ershoufang'][city].update({"_id":data['_id']},{"$set":{"status":True}})	
-				print("已经存在,不需要解析,添加状态信息")
+			except:
+				print("存储数据异常,删除该条数据,检查该条数据")
+				return
+			self.locationCol.insert(a)
+			self.oklength += 1
+			print("成功插入第%s条数据,解析的地址为:%s"%(self.oklength,data['address']))
+			self.sourceCol.update({"_id":data['_id']},{"$set":{"status":"OK"}})	
+		else:
+			self.sourceCol.update({"_id":data['_id']},{"$set":{"status":"OK"}})	
+			print("已经存在,不需要解析,状态设置为以解析")
+	#解析main方法
+	def parse(self,Number = None):
+		pool=gevent.pool.Pool(self.requestNum)
+		datas = self.sourceCol.find({"status":"SUBSPENDING"})
+		if Number:
+			datas = datas.limit(Number)
+		for data in datas:
+			greenlet = gevent.spawn(self.parseData,data)
+			pool.add(greenlet)
+		pool.join()
+		print("解析完毕,共成功%s条,失败%s条"%(self.oklength,self.errorlength))
 	def __del(self):
 		self.client.close()
 if __name__ == "__main__":
-	d = dumpLocation("127.0.0.1",27017)
-	d.parse("allhousedata","location")
-
+	starttime = datetime.datetime.now()
+	d = LocationDumper("127.0.0.1",
+										  27017,
+									   "ershoufang",
+									   "location",
+									   "ershoufang",
+								     "allhousedata",
+											requestNum = 50)
+	d.parse()
+	endtime = datetime.datetime.now()
+	print((endtime-starttime).seconds)
